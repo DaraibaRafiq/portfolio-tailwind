d4109d4be60c97615fe1af85eb3e18cb28b53967
DaraibaRafiq <daraibarafique8627@gmail.com>
2024-11-19 19:35:23 +0500

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="mt-16 py-6 text-center bg-accent">
+      <div>&#169; Daraiba| All rights reserved</div>
+      
+    </div>
+  )
+}
+
+export default Footer
